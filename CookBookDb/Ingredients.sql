24e7bb952803481678721faf07a2fcf8ebd26f8e
Neville Rabakale <neville.rabakale@gmail.com>
2022-12-28 11:16:21 +0100

    Add project files.

---
+++ CookBookDb/Ingredients.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Ingredients]
+(
+	[Id] INT NOT NULL PRIMARY KEY identity, 
+    [IngredientName] NVARCHAR(50) NOT NULL, 
+    [Amount] NVARCHAR(50) NOT NULL
+)
---
+++ CookBookDb/Recipe.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[Recipe]
+(
+	[Id] INT NOT NULL PRIMARY KEY identity, 
+    [RecipeName] NVARCHAR(50) NOT NULL, 
+    [RecipeType] NVARCHAR(50) NOT NULL, 
+    [Method] NVARCHAR(MAX) NOT NULL, 
+    [PortionSize] INT NULL, 
+    [CookingTime] INT NOT NULL, 
+    [TotalCalories] INT NULL, 
+    [Protein] INT NULL, 
+    [Carbohydrates] INT NULL, 
+    [Fats] INT NULL, 
+    [ImageUrl] NVARCHAR(50) NULL, 
+    [IngredientId] INT NULL
+
+)
